f53e6c866b0be977220c6e83b8e6543f728e79f8
Nikhil Anand <nikhilanand2001bam@gmail.com>
2025-03-26 02:02:25 +0530

    Book Management api's

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ export const app=express();
 import{errorMiddleware} from "./middlewares/errorMiddlewares.js";
 import authRouter from "./routes/authRouter.js";
 import bookRouter from "./routes/bookRouter.js";
+import borrowRouter from "./routes/borrowRouter.js";
 
 config({path: "./config/config.env"});
 
@@ -24,6 +25,7 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/book", bookRouter);
+app.use("/api/v1/borrow",borrowRouter);
 
 connectDB();
 
