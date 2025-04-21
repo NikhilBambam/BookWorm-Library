40a798a278197af8da18e01967903200cf7a5e2a
Nikhil Anand <nikhilanand2001bam@gmail.com>
2025-04-22 03:26:03 +0530

    Solved Bugs

--- server/routes/borrowRouter.js
+++ server/routes/borrowRouter.js
@@ -1,35 +1,12 @@
-// import express from "express";
-// import { isAuthenticated, isAuthorized} from '../middlewares/authMiddleware.js';
-// import {borrowedBooks,getBorrowedBooksForAdmin,recordBorrowedBook,returnBorrowBook} from "../controllers/borrowControllers.js";
-
-// const router = express.Router();
-
-// router.post("/record-borrow-book/:id",isAuthenticated,isAuthorized("Admin"),recordBorrowedBook);
-// router.get("/borrowed-books-by-users",isAuthenticated,isAuthorized("Admin"),getBorrowedBooksForAdmin);
-// router.get("/my-borrowed-books",isAuthenticated,borrowedBooks);
-// router.put("/return-borrowed-book/:bookId",isAuthenticated,isAuthorized("Admin"),returnBorrowBook);
-
-// export default router;
-
 import express from "express";
-import { isAuthenticated, isAuthorized } from '../middlewares/authMiddleware.js';
-import {
-  borrowedBooks,
-  getBorrowedBooksForAdmin,
-  recordBorrowedBook,
-  returnBorrowBook,
-  userBorrowBook // New controller for user-initiated borrows
-} from "../controllers/borrowControllers.js";
+import { isAuthenticated, isAuthorized} from '../middlewares/authMiddleware.js';
+import {borrowedBooks,getBorrowedBooksForAdmin,recordBorrowedBook,returnBorrowBook} from "../controllers/borrowControllers.js";
 
 const router = express.Router();
 
-// Admin-only routes
-router.post("/admin/record-borrow-book/:id", isAuthenticated, isAuthorized("Admin"), recordBorrowedBook);
-router.get("/admin/borrowed-books", isAuthenticated, isAuthorized("Admin"), getBorrowedBooksForAdmin);
-router.put("/admin/return-borrowed-book/:bookId", isAuthenticated, isAuthorized("Admin"), returnBorrowBook);
-
-// User routes
-router.post("/borrow-book/:id", isAuthenticated, isAuthorized("Borrower"), userBorrowBook);
-router.get("/my-borrowed-books", isAuthenticated, borrowedBooks);
+router.post("/record-borrow-book/:id",isAuthenticated,isAuthorized("Admin"),recordBorrowedBook);
+router.get("/borrowed-books-by-users",isAuthenticated,isAuthorized("Admin"),getBorrowedBooksForAdmin);
+router.get("/my-borrowed-books",isAuthenticated,borrowedBooks);
+router.put("/return-borrowed-book/:bookId",isAuthenticated,isAuthorized("Admin"),returnBorrowBook);
 
 export default router;
