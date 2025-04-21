3cf5086d69c153e8df84fe64036d1fdccea106f9
Nikhil Anand <nikhilanand2001bam@gmail.com>
2025-04-21 23:08:57 +0530

    Updated url

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,13 +1,32 @@
-export const sendToken = (user, statusCode, message, res) =>
-{
+// export const sendToken = (user, statusCode, message, res) =>
+// {
+//     const token = user.generateToken();
+//     res
+//     .status(statusCode)
+//     .cookie("token",token,{
+//         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 *60 * 60 * 1000),
+//         httpOnly:true,
+//     })
+
+//     .json({
+//         success: true,
+//         user,
+//         message,
+//         token,
+//     });
+// };
+
+export const sendToken = (user, statusCode, message, res) => {
     const token = user.generateToken();
+    
     res
     .status(statusCode)
-    .cookie("token",token,{
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 *60 * 60 * 1000),
-        httpOnly:true,
+    .cookie("token", token, {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true,        // Required for HTTPS (Render uses it)
+        sameSite: "None",    // Required for cross-origin cookies
     })
-
     .json({
         success: true,
         user,
@@ -15,3 +34,5 @@ export const sendToken = (user, statusCode, message, res) =>
         token,
     });
 };
+
+  
